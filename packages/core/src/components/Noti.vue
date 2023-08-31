a50b7fa35f60ea75d98d9bc69eb8f8a6411bc1d7
Rock070 <a0938130702@gmail.com>
2023-09-01 01:13:59 +0800

    feat(Noti): make multi notification

--- packages/core/src/components/Noti.vue
+++ packages/core/src/components/Noti.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
-import { reactive, ref } from 'vue'
-import { useToggle, watchImmediate } from '@vueuse/core'
+import { ref } from 'vue'
 import { useNoti } from '../composables/useNoti'
 import useEventBus from '../composables/useEventBus'
+import { DEFAULT_SETTING } from '../constant'
+
 import type { NotiOptions } from '../types'
 
-export type NotificationList = (NotiOptions & { id: symbol })[]
+export type NotificationList = (NotiOptions & { id: number | string | symbol })[]
 
 export interface NotiProps {
   test: string
@@ -16,65 +17,52 @@ defineProps<NotiProps>()
 const noti = useNoti()
 
 if (!noti)
-  console.warn('useNoti is not provided')
+  console.warn('[@vue3-noti/core warning] useNoti is undefined')
 
-const [open, toggleOpen] = useToggle(false)
+let count = 1
 
-const options = reactive({
-  message: '',
-  duration: 1000,
-})
-const message = ref('')
-const duration = ref(1000)
+const queue = ref<NotificationList>([])
 
 function publishEvent(options: NotiOptions) {
-  message.value = options.message
-  duration.value = options.duration
-  toggleOpen(true)
+  const item = { ...DEFAULT_SETTING, ...options, id: count++ }
+  queue.value.push(item)
+
+  const { id, duration } = item
+  setTimeout(() => {
+    queue.value = queue.value.filter(item => item.id !== id)
+  }, duration)
 }
 
 const bus = useEventBus()
 bus.on(publishEvent)
-
-function resetOptions() {
-  options.message = ''
-  options.duration = 1000
-}
-
-watchImmediate(
-  open,
-  (val) => {
-    if (!val)
-      return
-
-    setTimeout(() => {
-      toggleOpen(false)
-      resetOptions()
-    }, duration.value)
-  })
 </script>
 
 <template>
   <div class="vue3-noti">
-    <div v-if="open" class="vue3-noti-group">
-      {{ message }}
+    <div class="vue3-noti__container">
+      <div v-for="item in queue" :key="item.id" class="vue3-noti-group">
+        {{ item.message }} {{ item.id }}
+      </div>
     </div>
   </div>
 </template>
 
 <style>
-.vue3-noti .vue3-noti-group {
+.vue3-noti .vue3-noti__container {
   position: fixed;
   bottom: 16px;
   right: 16px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  gap: 16px;
+  z-index: 9999;
+}
+.vue3-noti .vue3-noti__container .vue3-noti-group {
   font-size: 32px;
   background-color: chartreuse;
   color: black;
   border-radius: 12px;
   padding: 10px 20px
-}
-
-.vue3-noti-group__item {
-
 }
 </style>
