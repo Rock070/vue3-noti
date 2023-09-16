7599f3fa515d9df582eba48d5155f8d7b8369e01
Rock070 <a0938130702@gmail.com>
2023-09-17 03:05:16 +0800

    feat(noti): distribute group

--- packages/core/src/components/Noti.vue
+++ packages/core/src/components/Noti.vue
@@ -7,7 +7,8 @@ import useEventBus from '../composables/useEventBus'
 
 import { useNotiContext } from '../composables/useNotiContext'
 
-import type { NotiOptions, Notification } from '../types'
+import type { NotiGroup, NotiOptions, Notification } from '../types'
+import { MS_PER_FRAME, POSITION_LIST } from '../constant'
 import AtomicProgress from './AtomicProgress.vue'
 
 export type NotificationList = Notification[]
@@ -15,12 +16,14 @@ export type NotificationList = Notification[]
 const { options: initialOptions } = useNotiContext()
 const { inc: countIncrease } = useCounter(1)
 
-const queue = ref<NotificationList>([])
-
-/**
- * Time per frame rendered (in milliseconds)
- */
-const MS_PER_FRAME = 1000 / 60
+const groupMap = ref<NotiGroup>({
+  'top-right': [],
+  'top-left': [],
+  'bottom-right': [],
+  'bottom-left': [],
+  'middle-top': [],
+  'middle-bottom': [],
+})
 
 function clearCountDown(timer: NodeJS.Timeout) {
   clearInterval(timer)
@@ -30,12 +33,13 @@ function closeNoti(val: Notification) {
   if (val.timer.intervalID !== undefined)
     clearCountDown(val.timer.intervalID)
 
-  queue.value = queue.value.filter(item => item.id !== val.id)
+  removeNotiFromGroup(val)
 }
 
 function triggerCountDown(target: Notification) {
   const interval = setInterval(() => {
-    const lastTime = target.timer.endCountDownTimestamp - Date.now()
+    const now = new Date()
+    const lastTime = target.timer.endCountDownTimestamp - now.getTime()
     if (lastTime > 0) {
       target.timer.lastTime = lastTime
       return
@@ -59,23 +63,52 @@ function makeNoti(options: NotiOptions): Notification {
       endCountDownTimestamp: 0,
     },
   }
+  const durationNum = Number(item.duration)
+  const duration = Number.isNaN(durationNum) ? 0 : durationNum
 
-  item.timer.lastTime = item.duration!
-  item.timer.endCountDownTimestamp = item.duration! + Date.now()
+  item.timer.lastTime = duration
+  item.timer.endCountDownTimestamp = duration + Date.now()
 
   return item
 }
 
+function findBelongGroup(val: Notification) {
+  if (!val.position || !POSITION_LIST.includes(val.position))
+    return
+
+  return groupMap.value[val.position]
+}
+
 function publishEvent(options: NotiOptions) {
   const item = makeNoti(options)
-  queue.value.push(item)
 
-  const target = queue.value.find(i => i.id === item.id)
+  const belongGroup = findBelongGroup(item)
+  if (!belongGroup) {
+    console.warn('Noti: Invalid position')
+    return
+  }
+
+  belongGroup.push(item)
+
+  const target = belongGroup.find(i => i.id === item.id)
 
   if (!target)
     return
 
   triggerCountDown(target)
+}
+
+function removeNotiFromGroup(val: Notification) {
+  const belongGroup = findBelongGroup(val)
+
+  if (!belongGroup)
+    return
+  const index = belongGroup.findIndex(item => item.id === val.id)
+
+  if (index === -1)
+    return
+
+  belongGroup.splice(index, 1)
 }
 
 function onMouseEnter(val: Notification) {
@@ -97,7 +130,16 @@ function onClick(val: Notification) {
     return
 
   clearCountDown(val.timer.intervalID)
-  queue.value = queue.value.filter(item => item.id !== val.id)
+
+  const belongGroup = findBelongGroup(val)
+  if (!belongGroup)
+    return
+  const index = belongGroup.findIndex(item => item.id === val.id)
+
+  if (index === -1)
+    return
+
+  belongGroup.splice(index, 0)
 }
 
 const bus = useEventBus()
@@ -106,38 +148,83 @@ bus.on(publishEvent)
 
 <template>
   <div class="vue3-noti">
-    <div class="vue3-noti__group">
+    <template
+      v-for="(group, position) in groupMap"
+      :key="position"
+    >
       <div
-        v-for="item in queue"
-        :key="item.id"
+        v-if="group.length > 0"
         class="vue3-noti-group"
-        @mouseenter="onMouseEnter(item)"
-        @mouseleave="onMouseLeave(item)"
-        @click="onClick(item)"
+        :class="[`vue3-noti-group--${position}`]"
       >
-        {{ item.message }}
-        <AtomicProgress
-          v-if="item.showProgressBar"
-          :value="item.timer.lastTime"
-          :max="item.duration"
-        />
+        <div
+          v-for="item in group"
+          :key="item.id"
+          class="vue3-noti-group__item"
+          :class="[`vue3-noti-group-item--${item.type}`]"
+          @mouseenter="onMouseEnter(item)"
+          @mouseleave="onMouseLeave(item)"
+          @click="onClick(item)"
+        >
+          {{ item.message }}
+          <AtomicProgress
+            v-if="item.showProgressBar"
+            :value="item.timer.lastTime"
+            :max="item.duration"
+          />
+        </div>
       </div>
-    </div>
+    </template>
   </div>
 </template>
 
 <style>
-.vue3-noti .vue3-noti__group {
+.vue3-noti {
+  --padding: 16px;
+}
+
+.vue3-noti .vue3-noti-group {
   position: fixed;
-  bottom: 16px;
-  right: 16px;
   display: flex;
   flex-direction: column;
   align-items: flex-end;
-  gap: 16px;
+  gap: var(--padding);
   z-index: 9999;
 }
-.vue3-noti .vue3-noti__group .vue3-noti-group {
+
+.vue3-noti .vue3-noti-group--top-right {
+  top: var(--padding);
+  right: var(--padding);
+}
+
+.vue3-noti .vue3-noti-group--top-left {
+  top: var(--padding);
+  left: var(--padding);
+}
+
+.vue3-noti .vue3-noti-group--bottom-right {
+  bottom: var(--padding);
+  right: var(--padding);
+}
+
+.vue3-noti .vue3-noti-group--bottom-left {
+  bottom: var(--padding);
+  left: var(--padding);
+}
+
+.vue3-noti .vue3-noti-group--middle-top {
+  left: 50%;
+  transform: translateX(-50%);
+  top: var(--padding);
+}
+
+.vue3-noti .vue3-noti-group--middle-bottom {
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: var(--padding);
+}
+
+.vue3-noti .vue3-noti-group .vue3-noti-group__item {
   position: relative;
   font-size: 32px;
   background-color: chartreuse;
--- packages/core/src/constant.ts
+++ packages/core/src/constant.ts
@@ -1,12 +1,26 @@
 import type { InjectionKey } from 'vue'
-import type { NotiContext, NotiOptions } from './types'
-import { NOTI_POSITION, NOTI_TYPE } from './types'
+import type { NotiContext, NotiOptions, NotiPositionTuple } from './types'
+import { NOTI_TYPE } from './types'
 
 export const DEFAULT_SETTING: NotiOptions = {
   message: '',
   type: NOTI_TYPE.SUCCESS,
-  position: NOTI_POSITION.TOP_RIGHT,
+  position: 'top-right',
   duration: 3000,
 }
 
 export const INJECT_KEY: InjectionKey<NotiContext> = Symbol('noti')
+
+export const POSITION_LIST: NotiPositionTuple = [
+  'top-right',
+  'top-left',
+  'bottom-right',
+  'bottom-left',
+  'middle-top',
+  'middle-bottom',
+]
+
+/**
+ * Time per frame rendered (in milliseconds)
+ */
+export const MS_PER_FRAME = 1000 / 60
