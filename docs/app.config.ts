6fadeb30ffe7476495b3724ed3b16bbc24a3fae5
Rock070 <a0938130702@gmail.com>
2023-09-18 02:32:44 +0800

    doc: add header, footer

---
+++ docs/app.config.ts
@@ -0,0 +1,24 @@
+export default defineAppConfig({
+  docus: {
+    socials: {
+      github: 'Rock070/vue3-noti',
+    },
+    aside: {
+      level: 1,
+    },
+    header: {
+      showLinkIcon: false,
+      logo: 'Logo',
+      exclude: ['/playground'],
+    },
+    footer: {
+      credits: {
+        icon: 'IconDocus',
+        text: 'Powered by Docus',
+        href: 'https://docus.dev',
+      },
+      textLinks: [],
+      iconLinks: [],
+    },
+  },
+})
