516cd13c1f33bcdaa2ab826a9522289c925f6bd4
Rock070 <a0938130702@gmail.com>
2023-09-25 02:10:43 +0800

    wip: provider

--- packages/core/src/plugins/noti.ts
+++ packages/core/src/plugins/noti.ts
@@ -3,7 +3,6 @@ import type { App, Plugin } from 'vue'
 import { useNotiProvideArguments } from '../composables/useNotiContext'
 import type { NotiOptions } from '../types'
 
-// app: App, options: any
 export const NotiPlugin: Plugin = {
   install: (app: App, initialOptions: NotiOptions) => {
     const notiArguments = useNotiProvideArguments(initialOptions)
@@ -11,3 +10,16 @@ export const NotiPlugin: Plugin = {
     app.provide(...notiArguments)
   },
 }
+
+// TODO: add global $noti type
+// declare module '@vue/runtime-core' {
+//   export interface ComponentCustomProperties {
+//     /**
+//      * Vue Final Modal global state for the modal components and also provides
+//      * functions that can be used to control the modal components. {@link Vfm}
+//      */
+//     $noti: ReturnType<typeof useNotiProvideArguments>
+//   }
+// }
+
+// export { }
